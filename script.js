f19e51f53927157e72eaf4595ecf524ef897285a
Sara <123300249+sarav929@users.noreply.github.com>
2024-10-21 15:57:00 +0100

    fix input to draw/erase from mouseover to hold click

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// set default values 
+// set default values
+ 
 const defaultMode = 'color';
 const defaultColor = '#000000';
 const defaultSize = 16;
@@ -10,6 +11,20 @@ const clear = document.getElementById('clear');
 const slider = document.getElementById("myRange");
 const output = document.getElementById("output");
 
+// variable to track clicked mouse
+
+let mouseDown = false;
+
+document.body.addEventListener('mousedown', () => {
+    mouseDown = true;
+});
+
+document.body.addEventListener('mouseup', () => {
+    mouseDown = false;
+});
+
+// create grid
+
 function createGrid(num) { 
     let gridArea = num * num;
     for (let i = 1; i <= gridArea; i++) {
@@ -18,36 +33,64 @@ function createGrid(num) {
         container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
         container.insertAdjacentElement('beforeend', square);
-        }
     }
+}
+
+// solid colour 
 
 function draw() {
     const boxes = Array. from(document.getElementsByClassName('box'));
     boxes.forEach((box) => {
+        box.addEventListener('mousedown', () => {
+            box.setAttribute('style', 'background-color:' + currentColor); 
+        });
+
         box.addEventListener('mouseover', () => {
-            box.setAttribute('style', 'background-color:' + currentColor);  
-        })
+            if (mouseDown) {
+                box.setAttribute('style', 'background-color:' + currentColor); 
+            }
+        });
     });
 }
+
+// erase 
 
 function erase() {
     const boxes = Array. from(document.getElementsByClassName('box'));
     boxes.forEach((box) => {
+        box.addEventListener('mousedown', () => {
+            box.setAttribute('style', 'background-color: transparent');  
+        });
+
         box.addEventListener('mouseover', () => {
-            box.setAttribute('style', 'background-color: none');       
-        })
+            if (mouseDown) {
+                box.setAttribute('style', 'background-color: transparent');  
+            }
+        });
     });
 }
 
+// rainbow colour
+
 function rainbow() {
     const boxes = Array. from(document.getElementsByClassName('box'));
+    let randomColor;
     boxes.forEach((box) => {
+        box.addEventListener('mousedown', () => {
+            randomColor = Math.floor(Math.random()*16777215).toString(16);
+            box.setAttribute('style', 'background-color: #' + randomColor);   
+        });
+
         box.addEventListener('mouseover', () => {
-            let randomColor = Math.floor(Math.random()*16777215).toString(16);
-            box.setAttribute('style', 'background-color: #' + randomColor);  
-        })
+            if (mouseDown) {
+                randomColor = Math.floor(Math.random()*16777215).toString(16);
+                box.setAttribute('style', 'background-color: #' + randomColor);  
+            }
+        });
     });
 }
+
+//clear grid 
 
 function clearGrid() {
     const boxes = Array. from(document.getElementsByClassName('box'));
@@ -57,6 +100,7 @@ function clearGrid() {
     };
 
 // color picker
+
 function pickColor() {
     const colorPicker = document.getElementById('colorpicker');
 
@@ -66,6 +110,7 @@ function pickColor() {
 }
 
 // start page with default values
+
 let currentMode = defaultMode;
 let currentColor = defaultColor;
 let currentSize = defaultSize;
@@ -75,6 +120,7 @@ createGrid(currentSize);
 pickColor();
 
 // pick canvas size with slider
+
 slider.oninput = function() {
     output.innerText = slider.value + ' x ' + slider.value
     currentSize = slider.value
@@ -83,6 +129,7 @@ slider.oninput = function() {
 }
 
 // select mode
+
 const buttons = document.querySelectorAll('button')
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
